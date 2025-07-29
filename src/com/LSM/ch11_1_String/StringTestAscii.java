cbea99435370b49a4b648569db841bb295572fca
LSM-1020 <smmlee@naver.com>
2025-07-29 17:57:44 +0900

    =

---
+++ src/com/LSM/ch11_1_String/StringTestAscii.java
@@ -0,0 +1,17 @@
+package com.LSM.ch11_1_String;
+
+public class StringTestAscii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char ch = '1';
+		
+		if (ch >=65 && ch<=90) { //65->대문자 A, 아스키코드값, 90-> 대문자 Z 아스키코드값
+			System.out.println(ch + "는 영문자 대문자 입니다");
+		} else if (ch >=97 && ch <= 122) {
+			System.out.println(ch + "는 영문자 대문자가 아닙니다");
+		}
+	}
+
+}
